454f203eb488223d7481b84d64845c20c5f12138
vivek <konavivekramakrishna@gmail.com>
2023-09-17 19:34:51 +0530

    PWA

--- src/index.tsx
+++ src/index.tsx
@@ -28,13 +28,13 @@ if (process.env.REACT_APP_SENTRY_DSN) {
 }
 
 const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
+  document.getElementById("root") as HTMLElement,
 );
 
 root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
 );
 serviceWorkerRegistration.unregister();
 // If you want to start measuring performance in your app, pass a function
